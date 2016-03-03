0f1b4872d05bea5cee69ae8be0af5f4200280d49
David Furnes <david@dfurnes.com>
2016-03-03 14:24:42 -0500

    Fix Lumen job mocking bug.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,4 +11,15 @@ class TestCase extends Laravel\Lumen\Testing\TestCase
     {
         return require __DIR__.'/../bootstrap/app.php';
     }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Resolve the Dispatcher, to fix a Lumen bug where mock gets overwritten
+        // the first time the class is resolved from the service container.
+        // @see: https://github.com/laravel/lumen-framework/issues/207
+        app('Illuminate\Contracts\Bus\Dispatcher');
+    }
+
 }

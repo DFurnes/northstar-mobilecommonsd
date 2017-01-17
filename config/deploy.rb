c68fcdf9a6986c9bd34a8daefcfd1d720bf0dce0
David Furnes <david@dfurnes.com>
2017-01-17 16:07:08 -0500

    Restart workers after a deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,9 +42,13 @@ namespace :deploy do
     run "cd #{release_path} && php artisan cache:clear"
   end
 
+  task :artisan_queue_restart do
+    run "cd #{release_path} && php artisan queue:restart"
+  end
 end
 
 after "deploy:update", "deploy:cleanup"
 after "deploy:symlink", "deploy:link_folders"
 after "deploy:link_folders", "deploy:artisan_migrate"
 after "deploy:artisan_migrate", "deploy:artisan_cache_clear"
+after "deploy:artisan_cache_clear", "deploy:artisan_queue_restart"
